b17bdeaf4c75035f2c47d3e2ecaf69c3d4711103
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-08 02:23:25 +0900

    4-13

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::thread;
+use std::time::Duration;
 fn main() {
     thread::spawn(|| {
-        println!("Thread: Start!");
-        println!("Thread: End!");
+        for n in 1..10 {
+            println!("Thread: No, {}.", n);
+            thread::sleep(Duration::from_millis(50));
+        }
     });
 
-    println!("Main: Start!");
-    println!("Main: End!");
+    for n in 1..10 {
+        println!("Main: No, {}.", n);
+        thread::sleep(Duration::from_millis(100));
+    }
 }
