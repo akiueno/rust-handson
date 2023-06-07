5174582c371ac89cfde28a3d7eef64e6e7c3cd1a
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-08 03:57:18 +0900

    4-22

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let (tx1, rx1) = mpsc::channel();
+    let (tx2, rx2) = mpsc::channel();
+    tx2.send(0).unwrap();
     println!("Main: start!");
 
     let h1 = thread::spawn(move || {
-        let mut num = 1;
         println!("H1: Start!");
 
-        for n in 1..5 {
-            num += n;
-            tx.send(num).unwrap();
+        for _n in 1..5 {
+            let val = rx2.recv().unwrap();
+            let num = val +1;
             println!("H1: num= {}", num);
+            tx1.send(num).unwrap();
             thread::sleep(Duration::from_millis(10));
         }
 
@@ -23,8 +25,10 @@ fn main() {
         println!("H2: Start!");
 
         for _n in 1..5 {
-            let num_rev = rx.recv().unwrap();
-            println!("H2: num= {}", num_rev);
+            let val = rx1.recv().unwrap();
+            let num = val * 2;
+            println!("H2: num= {}", num);
+            tx2.send(num).unwrap();
             thread::sleep(Duration::from_millis(20));
         }
 
