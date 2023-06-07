32cc9951c387e566078526723ad186f84578cc19
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-08 02:33:18 +0900

    4-16

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use std::thread;
 use std::time::Duration;
 fn main() {
-    thread::spawn(|| {
-        for n in 1..10 {
+    println!("Main: start!");
+
+    let h = thread::spawn(|| {
+
+        let h1 = thread::spawn(|| {
+            for n in 1..6 {
+                println!("H1: No, {}.", n);
+                thread::sleep(Duration::from_millis(2));
+            }
+        });
+
+        let h2 = thread::spawn(|| {
+            for n in 1..6 {
+                println!("H2: No, {}.", n);
+                thread::sleep(Duration::from_millis(2));
+            }
+        });
+
+        for n in 1..6 {
             println!("Thread: No, {}.", n);
-            thread::sleep(Duration::from_millis(50));
+            thread::sleep(Duration::from_millis(2));
         }
+
+        let _res1 = h1.join();
+        let _res2 = h2.join();
     });
 
-    for n in 1..10 {
-        println!("Main: No, {}.", n);
-        thread::sleep(Duration::from_millis(100));
-    }
+    let _res = h.join();
+    println!("Main: end!")
 }
