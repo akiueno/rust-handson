f632e74f3dc8bc090667342e42231a86d2c5a793
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-08 03:20:40 +0900

    4-18

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 fn main() {
+    let num = Arc::new(Mutex::new(1));
     println!("Main: start!");
+    let num_1 = Arc::clone(&num);
 
-    let h = thread::spawn(|| {
-
-        let h1 = thread::spawn(|| {
-            for n in 1..6 {
-                println!("H1: No, {}.", n);
-                thread::sleep(Duration::from_millis(2));
-            }
-        });
-
-        let h2 = thread::spawn(|| {
-            for n in 1..6 {
-                println!("H2: No, {}.", n);
-                thread::sleep(Duration::from_millis(2));
-            }
-        });
+    let h1 = thread::spawn(move || {
+        let mut num_h1 = num_1.lock().unwrap();
+        println!("H1: start!");
 
         for n in 1..6 {
-            println!("Thread: No, {}.", n);
-            thread::sleep(Duration::from_millis(2));
+            *num_h1 += n;
+            println!("H1: num_h={}.", *num_h1);
+            thread::sleep(Duration::from_millis(1));
         }
 
-        let _res1 = h1.join();
-        let _res2 = h2.join();
+        println!("H1: end!")
     });
 
-    let _res = h.join();
+    let num_2 = Arc::clone(&num);
+
+    let h2 = thread::spawn(move || {
+        let mut num_h2 = num_2.lock().unwrap();
+        println!("H2: start!");
+
+        for n in 1..6 {
+            *num_h2 *= n;
+            println!("H2: No, {}.", num_h2);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+        println!("H2: end!")
+    });
+
+    let _res1 = h1.join();
+    let _res2 = h2.join();
+
     println!("Main: end!")
 }
