3fd69547362490867d9a5291ec7b83087d8698a8
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-08 01:56:40 +0900

    4-3&4-4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
 fn main() {
-    println!("Hello, world!");
+    let mut data = vec![];
+
+    for _ in 0..10 {
+        data.push(random());
+    }
+
+    print_all(data);
+}
+
+fn random() -> Option<i32> {
+    let n = rand::thread_rng().gen_range(0..10);
+    match n {
+        0 => None,
+        _ => Some(n),
+    }
+}
+
+fn print_all(data: Vec<Option<i32>>) {
+    for item in data {
+        print(item);
+    }
+}
+
+fn print(item: Option<i32>) {
+    match item {
+        None => println!("no-data..."),
+        Some(n) => println!("No, {}", n),
+    }
 }
