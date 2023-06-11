285dd8b89582e905c0c0f26d57088ad6d4ac0813
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-11 15:09:14 +0900

    5-27

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ fn main() {
 }
 
 #[derive(Default)]
-struct MyEguiApp {
-}
+struct MyEguiApp {}
 
 impl MyEguiApp {
     fn new(_cc: &eframe::CreationContext) -> Self {
@@ -29,13 +28,18 @@ impl eframe::App for MyEguiApp {
 }
 
 fn plot(ui: &mut egui::Ui) {
-    let pos_1 = egui::Pos2::new(50.0, 50.0);
-    let pos_2 = egui::Pos2::new(200.0, 200.0);
-
-    let stroke_1 = egui::Stroke::new(5.0, egui::Color32::RED);
-    let stroke_2 = egui::Stroke::new(5.0, egui::Color32::GREEN);
-
-    ui.painter().vline(50.0, std::ops::RangeInclusive::new(50.0, 200.0), stroke_1);
-    ui.painter().hline(std::ops::RangeInclusive::new(50.0, 200.0), 50.0, stroke_1);
-    ui.painter().line_segment([pos_1, pos_2], stroke_2)
+    ui.painter().text(
+        egui::Pos2 { x: 50.0, y: 50.0 },
+        egui::Align2::LEFT_CENTER,
+        "Hello!",
+        egui::FontId::proportional(24.0),
+        egui::Color32::RED,
+    );
+    ui.painter().text(
+        egui::Pos2 { x: 50.0, y: 100.0 },
+        egui::Align2::LEFT_CENTER,
+        "Sample Message.",
+        egui::FontId::proportional(36.0),
+        egui::Color32::BLUE,
+    );
 }
