abc00d4c6c56251b5e91ec3368b289199940a13a
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-06-11 14:59:03 +0900

    5-25

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,13 @@ impl eframe::App for MyEguiApp {
 }
 
 fn plot(ui: &mut egui::Ui) {
-    let pos_1 = egui::Pos2::new(100.0, 100.0);
-    ui.painter().circle_filled(pos_1, 50.0, egui::Color32::RED);
+    let pos_1 = egui::Pos2::new(50.0, 50.0);
+    let pos_2 = egui::Pos2::new(200.0, 200.0);
 
-    let pos_2 = egui::Pos2::new(150.0, 150.0);
-    let stroke_2 = egui::Stroke::new(10.0, egui::Color32::GREEN);
-    ui.painter().circle_stroke(pos_2, 50.0, stroke_2);
+    let stroke_1 = egui::Stroke::new(5.0, egui::Color32::RED);
+    let stroke_2 = egui::Stroke::new(5.0, egui::Color32::GREEN);
+
+    ui.painter().vline(50.0, std::ops::RangeInclusive::new(50.0, 200.0), stroke_1);
+    ui.painter().hline(std::ops::RangeInclusive::new(50.0, 200.0), 50.0, stroke_1);
+    ui.painter().line_segment([pos_1, pos_2], stroke_2)
 }
